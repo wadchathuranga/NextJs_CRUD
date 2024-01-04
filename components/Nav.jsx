7c7fe5ce6937bf472ea21aa4644aea8d33163659
wadchathuranga <wa.dilshan.chathuranga@gmail.com>
2024-01-04 13:32:47 +0530

    next auth added

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -7,17 +7,17 @@ import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
 
-    const isUserLoggedIn = true;
+    const { data: session } = useSession();
 
     const [providers, setProviders] = useState(null);
     const [toggleDropdown, setToggleDropdown] = useState(false);
 
     useEffect(() => {
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
             setProviders(response);
-        }
-        setProviders();
+        };
+        setUpProviders();
     }, [])
 
   return (
@@ -36,7 +36,7 @@ const Nav = () => {
 
         {/* Desktop Navigation */}
         <div className="sm:flex hidden">
-            { isUserLoggedIn ? (
+            { session?.user ? (
                 <div className='flex gap-3 md:gap-5'>
                     <Link 
                         href='/create-prompt' 
@@ -51,7 +51,7 @@ const Nav = () => {
 
                     <Link href='/profile'>
                         <Image 
-                            src="/assets/images/logo.svg" 
+                            src={session?.user.image} 
                             alt="Profile"
                             width={37}
                             height={37}
@@ -77,10 +77,10 @@ const Nav = () => {
 
         {/* Mobile Navigation */}
         <div className="sm:hidden flex relative">
-            {isUserLoggedIn ? (
+            { session?.user ? (
                 <div className='flex'>
                     <Image 
-                        src="/assets/images/logo.svg" 
+                        src={session?.user.image} 
                         alt="Profile"
                         width={37}
                         height={37}
